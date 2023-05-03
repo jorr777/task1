c69e28d563820d9926a4790dd3613f77952953ef
Jorr <97128032+jorr777@users.noreply.github.com>
2023-05-04 02:06:50 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const header = document.querySelector('.header')
 
 menu.addEventListener('click' ,()=>{
    let res = header.classList.toggle('response')
+   let res2 = header.classList.toggle('header_col')
     nav.classList.toggle('response')
-    !res ? menu.style.border = 'none'  : menu.style.border = '1px solid black'
+    !res ? menu.style.border = 'none'  : menu.style.border = '1px solid #707070b5'
 })
 
 
